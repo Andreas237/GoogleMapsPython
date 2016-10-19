eff61edb9b2ccec18e63c889ded203895b82f1ba
Andreas237 <silenta237@gmail.com>
2016-10-18 22:56:35 -0700

    Geometry complete. Internet connection failing... No access to needed data. Next: PROCESSCITIES to loop through pairs, and call the Vicenty function on their coordinates.  Get coordinates with  getCityGeo.  Store min result in currentMin

--- geometry.py
+++ geometry.py
@@ -31,7 +31,12 @@ class geometry:
     # This special case works for all distances
     # Input: (latitude, longitude pairs) as Phi and Lam respectively
     # Output: the central angle of the two points
-    def VicentyCentralAngle( phi1, phi2, lam1, lam2 ):
+    def VicentyCentralAngle( c0, c1 ):
+        # c = [phi1, lam1]
+        phi1 = c0[0]
+        lam1 = c0[1]
+        phi2 = c1[0]
+        lam2 = c1[1]
         dPhi = math.radians( phi1 - phi2)
         dLam = math.radians( lam1 - lam2)
         phi1 = math.radians( phi1)
@@ -40,6 +45,15 @@ class geometry:
         denominator = math.sin(phi1)*math.sin(phi2)+math.cos(phi1)*math.cos(phi2)*math.cos(dLam)
         return math.atan( math.sqrt( radicand ) / denominator )
 
+
+    def VicentyCentralAngle2( phi1, phi2, lam1, lam2 ):
+        dPhi = math.radians( phi1 - phi2)
+        dLam = math.radians( lam1 - lam2)
+        phi1 = math.radians( phi1)
+        phi2 = math.radians( phi2)
+        radicand = ( math.cos(phi2)*math.sin(dLam))**2 + (math.cos(phi1)*math.sin(phi2)-math.sin(phi1)*math.cos(phi2)*math.cos(dLam))**2
+        denominator = math.sin(phi1)*math.sin(phi2)+math.cos(phi1)*math.cos(phi2)*math.cos(dLam)
+        return math.atan( math.sqrt( radicand ) / denominator )
 
 
     # Arc Length from the Law of Cosines
@@ -55,8 +69,8 @@ class geometry:
 #######################################################################################################
 
 print(geometry.ArcLength(1,90))
+c0 = [36.12,-86.67]
+c1 = [ 33.94, -118.40 ]
 
-print( geometry.ArcLength( 6372.8 , geometry.HaversineCentralAngle(36.12, 33.94, -86.67, -118.40)) )
-print( 6372.8 * geometry.HaversineCentralAngle(36.12, 33.94, -86.67, -118.40) )
-print( geometry.ArcLength( 6372.8 , geometry.VicentyCentralAngle(36.12, 33.94, -86.67, -118.40)) )
-print( 6372.8 * geometry.VicentyCentralAngle(36.12, 33.94, -86.67, -118.40) )
+print( 6372.8 * geometry.VicentyCentralAngle2(36.12, 33.94, -86.67, -118.40) )
+print( 6372.8 * geometry.VicentyCentralAngle(  c0, c1) )
